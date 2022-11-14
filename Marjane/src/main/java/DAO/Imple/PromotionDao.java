1403e6d5472a2fb5d933a827c27500263a8a2665
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-11-14 16:32:06 +0100

    Promotion DAO

---
+++ Marjane/src/main/java/DAO/Imple/PromotionDao.java
@@ -0,0 +1,86 @@
+package DAO.Imple;
+
+import DAO.BaseDao;
+import Entity.Promotion;
+import jakarta.persistence.*;
+
+import java.util.Optional;
+import java.util.List;
+
+public class PromotionDao implements BaseDao<Promotion>{
+
+    EntityManagerFactory entityManagerFactory;
+    EntityManager entityManager;
+    EntityTransaction entityTransaction;
+
+    public PromotionDao(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        entityManager = entityManagerFactory.createEntityManager();
+        entityTransaction = entityManager.getTransaction();
+    }
+
+    @Override
+    public Optional<Promotion> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Promotion.class, id));
+    }
+
+    @Override
+    public Promotion save(Promotion promotion) {
+        try {
+            entityTransaction.begin();
+            if(promotion == null){
+                entityManager.persist(null);
+            }
+            entityManager.merge(promotion);
+            entityTransaction.commit();
+            return promotion;
+        } catch (Exception e) {
+            entityTransaction.rollback();
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return promotion;
+    }
+
+    @Override
+    public List<Promotion> all() {
+        return entityManager.createQuery("from Promotion ", Promotion.class).getResultList();
+    }
+
+    @Override
+    public Boolean update(Long id, Promotion promotion) {
+        try {
+            entityTransaction.begin();
+            String hql = "UPDATE Promotion set status = :status " + "WHERE id = :id";
+            Query qry = entityManager.createQuery(hql);
+            qry.setParameter("status", promotion.getStatus());
+            qry.setParameter("id", id);
+            entityTransaction.commit();
+            return true;
+        } catch (Exception e) {
+            entityTransaction.rollback();
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        try{
+            entityTransaction.begin();
+            Optional<Promotion> promotion = findById(id);
+            promotion.ifPresent(pro -> entityManager.remove(promotion));
+            entityTransaction.commit();
+        }catch (Exception e){
+            entityTransaction.rollback();
+            return false;
+        } finally {
+            entityManager.close();
+        }
+        return true;
+    }
+
+}
